809f78a2f9f0a722eb312f2e4a9512131acc189d
Ben Smith <bensmithers2000@gmail.com>
2022-10-17 21:03:21 -0400

    ben and jack initial segue - had a lot of trouble getting the simulator to run so we might wanna work on this more tomorrow with someone who can actually run the simulator

--- Pocket Pantry/Pocket Pantry/ViewController.swift
+++ Pocket Pantry/Pocket Pantry/ViewController.swift
@@ -9,10 +9,16 @@ import UIKit
 import Firebase
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var viewPantry: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func showPantry(_ sender:UIButton) {
+        performSegue(withIdentifier: "tableSegue", sender: self)
     }
 }
 
